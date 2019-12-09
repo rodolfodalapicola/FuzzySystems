634a1ce649e8336aa89f0d2bca5e5ab2bca290fe
Rodolfo <rodolfo.dalapicola@usp.br>
2019-12-09 02:33:52 -0300

    Update fuzzyLib.py

--- fuzzyLib.py
+++ fuzzyLib.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 DISC_POINTS = 1000
-FUZZY_LIMIT = 50
+FUZZY_LIMIT = 14
 
 
 def idx(x):
@@ -20,7 +20,7 @@ def plotAll(space, conjs):
         plt.plot(space, conjs)
     else:
         for conj in conjs:
-            plt.plot(space, conj, '-')
+            plt.plot(space, conj, '*')
     ax.set_ylim([0, 1.1])
     plt.show()
 
@@ -33,8 +33,10 @@ def makeTriangular(linear_start, linear_top):
     a = (linear_top - linear_start) / 1
     x = np.zeros(DISC_POINTS)
     for i in range(idx(linear_top - linear_start)):
-        x[i+idx(linear_start)] = nmb(i) / a
-        x[i+idx(linear_top)] = 1 - nmb(i) / a
+        if(0 <= i+idx(linear_start) < DISC_POINTS):
+            x[i+idx(linear_start)] = nmb(i) / a
+        if(0 <= i+idx(linear_top) < DISC_POINTS):
+            x[i+idx(linear_top)] = 1 - nmb(i) / a
     return x
 
 
@@ -156,13 +158,21 @@ def max_min(rxy, ryz):
 # DAQUI PARA BAIXO ESTAO OS REQUERIMENTOS DO EPC 2
 # ----------------------------------------------------------------------------------------------------------------------
 def singleton(conj, x):
-    output = np.zeros([1, len(conj)])
-    output[0, idx(x)] = 1
+    output = np.zeros(len(conj))
+    output[idx(x)] = 1
     return output
 
 
 def mamdani(conj_a, conj_b):
     output = np.minimum(np.expand_dims(conj_a, axis=1), np.expand_dims(conj_b, axis=0))
+    return output
+
+def minimum(conj_a, conj_b):
+    output = np.minimum(conj_a, conj_b)
+    return output
+
+def mamdani2(conj_a, conj_b):
+    output = np.minimum(conj_b, conj_a)
     return output
 
 
@@ -176,3 +186,139 @@ def zadeh(conj_a, conj_b):
 def larsen(conj_a, conj_b):
     output = np.outer(conj_a, conj_b)
     return output
+
+# ----------------------------------------------------------------------------------------------------------------------
+# DAQUI PARA BAIXO ESTAO OS REQUERIMENTOS DO EPC 7 e do projeto
+# ----------------------------------------------------------------------------------------------------------------------
+def getAlfas(fuzzy_a, fuzzy_b, alfa):
+    a1 = 0
+    b1 = 0
+    a2 = 0
+    b2 = 0
+    for x in range(len(fuzzy_a)):
+        if (fuzzy_a[x] > alfa):
+            a1 = x
+            break
+    for x in range(a1+1, len(fuzzy_a)):
+        if (fuzzy_a[x] < alfa):
+            b1 = x
+            break
+    for x in range(len(fuzzy_b)):
+        if (fuzzy_b[x] > alfa):
+            a2 = x
+            break
+    for x in range(a2+1, len(fuzzy_b)):
+        if (fuzzy_b[x] < alfa):
+            b2 = x
+            break
+    return a1, b1, a2, b2
+
+def sumFuzzy(fuzzy_a, fuzzy_b, alfa_num):
+    step = 1/alfa_num
+    output = np.zeros(len(fuzzy_a))
+    for i in range(alfa_num):
+        a1, b1, a2, b2 = getAlfas(fuzzy_a, fuzzy_b, i*step)
+        if a1+a2 < len(fuzzy_a):
+            output[a1+a2] = i * step
+        if b1+b2 < len(fuzzy_a):
+            output[b1+b2] = i * step
+    return output
+
+def subFuzzy(fuzzy_a, fuzzy_b, alfa_num):
+    step = 1/alfa_num
+    output = np.zeros(len(fuzzy_a))
+    over_0 = 0
+    total = 0
+    for i in range(alfa_num):
+        alfa_corte = i * step
+        a1, b1, a2, b2 = getAlfas(fuzzy_a, fuzzy_b, alfa_corte)
+        if a1-b2 >= 0:
+            output[a1-b2] = alfa_corte
+        if b1-a2 >= 0:
+            output[b1-a2] = alfa_corte
+
+    med_area = 0
+    for i in range(len(output)):
+        med_area += output[i]
+
+    med_area = med_area / 2
+    soma = 0
+    indice_goal = 0
+    for i in range(len(output)):
+        soma += output[i]
+        if (soma > med_area):
+            indice_goal = i
+            break
+
+    return output, indice_goal
+
+def subConst(fuzzy, cnst):
+    output = np.zeros(len(fuzzy))
+
+    med_area = 0
+    for i in range(cnst, len(output)):
+        med_area += fuzzy[i]
+
+    med_area = med_area / 2
+
+    soma = 0
+    indice_goal = 0
+    for i in range(cnst, len(output)):
+        soma += fuzzy[i]
+        if (soma > med_area):
+            indice_goal = i
+            break
+    output = output + getMembership(indice_goal, fuzzy)
+    return output, indice_goal
+
+def minFuzzy(fuzzy_a, fuzzy_b, alfa_num):
+    step = 1/alfa_num
+    output = np.zeros(len(fuzzy_a))
+    over_0 = 0
+    total = 0
+    for i in range(alfa_num):
+        alfa_corte = i * step
+        a1, b1, a2, b2 = getAlfas(fuzzy_a, fuzzy_b, alfa_corte)
+        output[min(a1, a2)] = alfa_corte
+        output[min(b1, b2)] = alfa_corte
+
+    return output
+
+def maxFuzzy(fuzzy_a, fuzzy_b, alfa_num):
+    step = 1/alfa_num
+    output = np.zeros(len(fuzzy_a))
+    over_0 = 0
+    total = 0
+    for i in range(alfa_num):
+        alfa_corte = i * step
+        a1, b1, a2, b2 = getAlfas(fuzzy_a, fuzzy_b, alfa_corte)
+        output[max(a1, a2)] = alfa_corte
+        output[max(b1, b2)] = alfa_corte
+
+    return output
+
+def prodFuzzy(fuzzy_a, fuzzy_b, alfa_num):
+    step = 1/alfa_num
+    output = np.zeros(len(fuzzy_a))
+    for i in range(alfa_num):
+        a1, b1, a2, b2 = getAlfas(fuzzy_a, fuzzy_b, i*step)
+        if idx(nmb(a1)*nmb(a2)) < len(fuzzy_a):
+            output[idx(nmb(a1)*nmb(a2))] = i * step
+        if idx(nmb(b1)*nmb(b2)) < len(fuzzy_a):
+            output[idx(nmb(b1)*nmb(b2))] = i * step
+    return output
+
+def divFuzzy(fuzzy_a, fuzzy_b, alfa_num):
+    step = 1/alfa_num
+    output = np.zeros(len(fuzzy_a))
+    for i in range(alfa_num):
+        a1, b1, a2, b2 = getAlfas(fuzzy_a, fuzzy_b, i*step)
+        b2 += 1
+        if idx(nmb(a1)/nmb(b2)) < len(fuzzy_a):
+            output[idx(a1/b2)] = i * step
+        if idx(nmb(b2)/nmb(a1)) < len(fuzzy_a):
+            output[idx(nmb(b2)/nmb(a1))] = i * step
+    return output
+
+def defuzzify(fuzzy):
+    return max(fuzzy) 
